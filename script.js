cbfe443d37b8ed1d3834876b1cca4f98b26940a8
Fernando Ordonez <fernando@nuli.app>
2024-08-18 00:55:17 +0800

    Initial Commit

--- script.js
+++ script.js
@@ -1,5 +1,61 @@
 document.addEventListener("DOMContentLoaded", () => {
+    let clickCount = 0;
     const birthday = new Date("2025-01-09");
+    const header = document.querySelector("header");
+    const magicButton = document.querySelector("#magic-button");
+    const clickMe = document.getElementById("click-me");
+    const clickCountText = document.getElementById("click-count");
+
+    const hideFamily = document.getElementById("hide-family");
+    const familyDetails = document.getElementById("family-details");
+
+    const hideFavorites = document.getElementById("hide-favorites");
+    const favoritesDetails = document.getElementById("favorites-details");
+
+    const summerVacationDetails = document.getElementById(
+        "summer-vacation-details"
+    );
+
+    document.addEventListener("keypress", (e) => {
+        if (e.key === "~") {
+            if (summerVacationDetails.style.display === "none") {
+                summerVacationDetails.style.display = "flex";
+            } else {
+                summerVacationDetails.style.display = "none";
+            }
+        }
+    });
+
+    hideFavorites.addEventListener("dblclick", () => {
+        if (favoritesDetails.style.display === "none") {
+            favoritesDetails.style.display = "flex";
+            hideFavorites.textContent = "Hide";
+        } else {
+            favoritesDetails.style.display = "none";
+            hideFavorites.textContent = "Show";
+        }
+    });
+
+    hideFamily.addEventListener("click", () => {
+        if (familyDetails.style.display === "none") {
+            familyDetails.style.display = "flex";
+            hideFamily.textContent = "Hide";
+        } else {
+            familyDetails.style.display = "none";
+            hideFamily.textContent = "Show";
+        }
+    });
+
+    magicButton.addEventListener("click", () => {
+        const randomColor =
+            "#" + Math.floor(Math.random() * 16777215).toString(16);
+        header.style.backgroundColor = randomColor;
+    });
+
+    clickMe.addEventListener("click", () => {
+        clickCount = clickCount + 1;
+        clickCountText.textContent = clickCount;
+    });
 
     function updateCountdown() {
         const daysElement = document.querySelector("#days");
